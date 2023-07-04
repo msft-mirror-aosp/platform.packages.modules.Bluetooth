a32f9b68f5cc50a783320787e196fc8713cf1634
Sungsoo Lim <sungsoo@google.com>
2023-07-04 18:00:09 +0900

    Stop using intent from A2DP profile to ActiveDeviceManager
    Bug: 285981195
    Bug: 285959170
    Test: atest BluetoothInstrumentationTests
    (cherry picked from https://android-review.googlesource.com/q/commit:59ee0723fe7a76ff059dad74b9aaa64bfb6f10ba)
    Merged-In: I21fcb10d35802cb1b19f4925d63822bac54cbd8e
    Change-Id: I21fcb10d35802cb1b19f4925d63822bac54cbd8e

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -707,6 +707,10 @@ public class AdapterService extends Service {
         }
     }
 
+    public ActiveDeviceManager getActiveDeviceManager() {
+        return mActiveDeviceManager;
+    }
+
     private boolean initMetricsLogger() {
         if (mMetricsLogger != null) {
             return false;
