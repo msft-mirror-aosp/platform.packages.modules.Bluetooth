miyu-xu/packages-modules-Bluetooth

Retry SDP when we receive an SDP_BUSY response
Sometimes the stack can return an SDP_BUSY response, meaning we sent a
valid request to the stack but it couldn't be handled at this time. The
native stack doesn't enqueue or handle this event.

To fix this, we can simply retry SDP when we get an SDP_BUSY (2)
response from the native stack.

Tag: #stability
Bug: 293911750
Test: atest BluetoothInstrumentationTests
Change-Id: I9e8afd94f4e26705fdb062c49034f6851f11e098

android/app/src/com/android/bluetooth/mapclient/MceStateMachine.java
@@ -116,8 +116,14 @@ class MceStateMachine extends StateMachine {
     private static final int MAX_MESSAGES = 20;
     private static final int MSG_CONNECT = 1;
     private static final int MSG_DISCONNECT = 2;
-    private static final int MSG_CONNECTING_TIMEOUT = 3;
+    static final int MSG_CONNECTING_TIMEOUT = 3;
     private static final int MSG_DISCONNECTING_TIMEOUT = 4;
+
+    // Constants for SDP. Note that these values come from the native stack, but no centralized
+    // constants exist for them as part of the various SDP APIs.
+    public static final int SDP_SUCCESS = 0;
+    public static final int SDP_FAILED = 1;
+    public static final int SDP_BUSY = 2;
 
     private static final boolean MESSAGE_SEEN = true;
     private static final boolean MESSAGE_NOT_SEEN = false;
@@ -291,6 +297,21 @@ class MceStateMachine extends StateMachine {
 
     public synchronized int getState() {
         return mMostRecentState;
+    }
+
+    /**
+     * Notify of SDP completion.
+     */
+    public void sendSdpResult(int status, SdpMasRecord record) {
+        if (DBG) {
+            Log.d(TAG, "Received SDP Result, status=" + status + ", record=" + record);
+        }
+        if (status != SDP_SUCCESS || record == null) {
+            Log.w(TAG, "SDP failed, status: " + status + ", record: " + record);
+            sendMessage(MceStateMachine.MSG_MAS_SDP_FAILED, status);
+            return;
+        }
+        sendMessage(MceStateMachine.MSG_MAS_SDP_DONE, record);
     }
 
     public boolean disconnect() {
@@ -530,6 +551,28 @@ class MceStateMachine extends StateMachine {
                         }
                         mMasClient = new MasClient(mDevice, MceStateMachine.this, record);
                         setDefaultMessageType(record);
+                    }
+                    break;
+
+                case MSG_MAS_SDP_FAILED:
+                    int sdpStatus = message.arg1;
+                    Log.i(TAG, Utils.getLoggableAddress(mDevice)
+                            + " [Connecting]: SDP failed, status=" + sdpStatus);
+                    if (sdpStatus == SDP_BUSY) {
+                        if (DBG) {
+                            Log.d(TAG, Utils.getLoggableAddress(mDevice)
+                                    + " [Connecting]: SDP was busy, try again");
+                        }
+                        mDevice.sdpSearch(BluetoothUuid.MAS);
+                    } else {
+                        // This means the status is 0 (success, but no record) or 1 (organic
+                        // failure). We historically have never retried SDP in failure cases, so we
+                        // don't need to wait for the timeout anymore.
+                        if (DBG) {
+                            Log.d(TAG, Utils.getLoggableAddress(mDevice)
+                                    + " [Connecting]: SDP failed completely, disconnecting");
+                        }
+                        transitionTo(mDisconnecting);
                     }
                     break;
 

msft-mirror-aosp/platform.packages.modules.Bluetooth

Ignore-AOSP-First: This is a CP from AOSP
(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:6a6763ca84d31003efc6553d306d8fe5112f8adb)
Merged-In: I9e8afd94f4e26705fdb062c49034f6851f11e098

android/app/src/com/android/bluetooth/mapclient/MceStateMachine.java
@@ -93,7 +93,7 @@ class MceStateMachine extends StateMachine {
     static final int MSG_MAS_REQUEST_COMPLETED = 1003;
     static final int MSG_MAS_REQUEST_FAILED = 1004;
     static final int MSG_MAS_SDP_DONE = 1005;
-    static final int MSG_MAS_SDP_FAILED = 1006;
+    static final int MSG_MAS_SDP_UNSUCCESSFUL = 1006;
     static final int MSG_OUTBOUND_MESSAGE = 2001;
     static final int MSG_INBOUND_MESSAGE = 2002;
     static final int MSG_NOTIFICATION = 2003;
@@ -115,8 +115,14 @@ class MceStateMachine extends StateMachine {
     private static final int MAX_MESSAGES = 20;
     private static final int MSG_CONNECT = 1;
     private static final int MSG_DISCONNECT = 2;
-    private static final int MSG_CONNECTING_TIMEOUT = 3;
+    static final int MSG_CONNECTING_TIMEOUT = 3;
     private static final int MSG_DISCONNECTING_TIMEOUT = 4;
+
+    // Constants for SDP. Note that these values come from the native stack, but no centralized
+    // constants exist for them as part of the various SDP APIs.
+    public static final int SDP_SUCCESS = 0;
+    public static final int SDP_FAILED = 1;
+    public static final int SDP_BUSY = 2;
 
     private static final boolean MESSAGE_SEEN = true;
     private static final boolean MESSAGE_NOT_SEEN = false;
@@ -283,6 +289,21 @@ class MceStateMachine extends StateMachine {
 
     public synchronized int getState() {
         return mMostRecentState;
+    }
+
+    /**
+     * Notify of SDP completion.
+     */
+    public void sendSdpResult(int status, SdpMasRecord record) {
+        if (DBG) {
+            Log.d(TAG, "Received SDP Result, status=" + status + ", record=" + record);
+        }
+        if (status != SDP_SUCCESS || record == null) {
+            Log.w(TAG, "SDP unsuccessful, status: " + status + ", record: " + record);
+            sendMessage(MceStateMachine.MSG_MAS_SDP_UNSUCCESSFUL, status);
+            return;
+        }
+        sendMessage(MceStateMachine.MSG_MAS_SDP_DONE, record);
     }
 
     public boolean disconnect() {
@@ -522,6 +543,28 @@ class MceStateMachine extends StateMachine {
                         }
                         mMasClient = new MasClient(mDevice, MceStateMachine.this, record);
                         setDefaultMessageType(record);
+                    }
+                    break;
+
+                case MSG_MAS_SDP_UNSUCCESSFUL:
+                    int sdpStatus = message.arg1;
+                    Log.i(TAG, Utils.getLoggableAddress(mDevice)
+                            + " [Connecting]: SDP unsuccessful, status=" + sdpStatus);
+                    if (sdpStatus == SDP_BUSY) {
+                        if (DBG) {
+                            Log.d(TAG, Utils.getLoggableAddress(mDevice)
+                                    + " [Connecting]: SDP was busy, try again");
+                        }
+                        mDevice.sdpSearch(BluetoothUuid.MAS);
+                    } else {
+                        // This means the status is 0 (success, but no record) or 1 (organic
+                        // failure). We historically have never retried SDP in failure cases, so we
+                        // don't need to wait for the timeout anymore.
+                        if (DBG) {
+                            Log.d(TAG, Utils.getLoggableAddress(mDevice)
+                                    + " [Connecting]: SDP failed completely, disconnecting");
+                        }
+                        transitionTo(mDisconnecting);
                     }
                     break;
 
@@ -1170,8 +1213,8 @@ class MceStateMachine extends StateMachine {
                 return "MSG_MAS_REQUEST_FAILED";
             case MSG_MAS_SDP_DONE:
                 return "MSG_MAS_SDP_DONE";
-            case MSG_MAS_SDP_FAILED:
-                return "MSG_MAS_SDP_FAILED";
+            case MSG_MAS_SDP_UNSUCCESSFUL:
+                return "MSG_MAS_SDP_UNSUCCESSFUL";
             case MSG_OUTBOUND_MESSAGE:
                 return "MSG_OUTBOUND_MESSAGE";
             case MSG_INBOUND_MESSAGE: