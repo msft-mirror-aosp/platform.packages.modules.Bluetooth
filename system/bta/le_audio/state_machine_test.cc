6a565ab925492b6de2221a81a7ef1081d271a40e
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2024-07-24 16:03:38 +0000

    le_audio: state_machine: Fix Data Path not being removed
    This fixes bug when Data Path being in CONFIGURING state was not removed.
    CONFIGURING state means the Setup ISO Data Path complete event is pending.
    
    Bug: 393101186
    Flag: Exempt; regression covered by unit test added
    Test: atest bluetooth_le_audio_test
    Change-Id: I5d295245aeb8072bd8cc032d33dc79c7803ba10c

--- system/bta/le_audio/state_machine_test.cc
+++ system/bta/le_audio/state_machine_test.cc
@@ -238,6 +238,7 @@ protected:
   bool do_not_send_cis_establish_event_;
   bool do_not_send_cis_disconnected_event_;
   bool do_not_send_setup_iso_data_path_event_;
+  bool do_not_send_remove_iso_data_path_event_;
   uint8_t overwrite_cis_status_idx_;
   std::vector<uint8_t> cis_status_;
 
@@ -269,6 +270,7 @@ protected:
     do_not_send_cis_establish_event_ = false;
     do_not_send_cis_disconnected_event_ = false;
     do_not_send_setup_iso_data_path_event_ = false;
+    do_not_send_remove_iso_data_path_event_ = false;
     stay_in_releasing_state_ = false;
     block_releasing_state_device_list_.clear();
     stop_inject_configured_ase_after_first_ase_configured_ = false;
@@ -444,6 +446,11 @@ protected:
               log::debug("RemoveIsoDataPath");
 
               ASSERT_NE(conn_handle, kInvalidCisConnHandle);
+
+              if (do_not_send_remove_iso_data_path_event_) {
+                log::debug("Don't send remove ISO data path event");
+                return;
+              }
 
               auto dev_it = std::find_if(le_audio_devices_.begin(), le_audio_devices_.end(),
                                          [&conn_handle](auto& dev) {
@@ -9861,6 +9868,114 @@ TEST_F(StateMachineTest, testLateSetupIsoDatPathCompleteEvent) {
   log::debug("[TESTING] ProcessHciNotifSetupIsoDataPath. Expect StatusReportCb to be called");
   LeAudioGroupStateMachine::Get()->ProcessHciNotifSetupIsoDataPath(group, firstDevice, 0,
                                                                    cis_conn_handle);
+}
+
+TEST_F(StateMachineTest, testRemoveIsoDataPathOnCisDisconnection) {
+  const auto context_type = kContextTypeRingtone;
+  const auto audio_contexts = types::AudioContexts(context_type);
+  const auto group_id = 4;
+  const auto num_devices = 2;
+
+  ContentControlIdKeeper::GetInstance()->SetCcid(media_context, media_ccid);
+
+  /**
+   * Scenario:
+   * 1. Having set of 2 devices start streaming to 1 device.
+   * 2. Verify the group is streaming.
+   * 3. Attach the other device.
+   * 4. Central successfully creates CIS and issues HCI LE Setup ISO Data Path command.
+   * 5. The Data Path is not been set up yet. while Peripheral notifies ASE Releasing state.
+   * 6. The Central disconnects CIS and issues HCI LE Remove ISO Data Path command.
+   */
+
+  log::debug("[TESTING] Prepare 2 fake connected devices in a group");
+  auto* group = PrepareSingleTestDeviceGroup(group_id, context_type, num_devices);
+  ASSERT_NE(nullptr, group);
+  ASSERT_EQ(group->Size(), num_devices);
+
+  auto* firstDevice = group->GetFirstDevice();
+  EXPECT_CALL(gatt_queue, WriteCharacteristic(firstDevice->conn_id_, firstDevice->ctp_hdls_.val_hdl,
+                                              _, GATT_WRITE_NO_RSP, _, _))
+          .Times(AtLeast(3));
+  ASSERT_NE(nullptr, firstDevice);
+
+  auto* secondDevice = group->GetNextDevice(firstDevice);
+  EXPECT_CALL(gatt_queue,
+              WriteCharacteristic(secondDevice->conn_id_, secondDevice->ctp_hdls_.val_hdl, _,
+                                  GATT_WRITE_NO_RSP, _, _))
+          .Times(AtLeast(3));
+
+  log::debug("[TESTING] firstDevice notifies there are no available context at the time");
+  DeviceContextsUpdate(firstDevice, types::kLeAudioDirectionSink, types::AudioContexts(),
+                       audio_contexts);
+
+  PrepareConfigureCodecHandler(group, 0, true);
+  PrepareConfigureQosHandler(group);
+  PrepareEnableHandler(group, 0, /* inject_enabling */ true, /* inject_streaming */ true);
+  PrepareDisableHandler(group);
+  PrepareReleaseHandler(group);
+
+  // StartStream action initiated by upper layer
+  log::debug("[TESTING] StartStream. Expect STREAMING state to be not reported");
+  ASSERT_TRUE(LeAudioGroupStateMachine::Get()->StartStream(
+          group, context_type,
+          {.sink = types::AudioContexts(context_type),
+           .source = types::AudioContexts(context_type)}));
+
+  ASSERT_EQ(group->GetState(), types::AseState::BTA_LE_AUDIO_ASE_STATE_STREAMING);
+
+  PrepareEnableHandler(group, 0, /* inject_enabling */ true, /* inject_streaming */ false);
+
+  uint16_t cis_conn_handle;
+  EXPECT_CALL(*mock_iso_manager_, SetupIsoDataPath(::testing::_, ::testing::_))
+          .WillOnce(::testing::SaveArg<0>(&cis_conn_handle));
+  EXPECT_CALL(*mock_iso_manager_, EstablishCis(_)).Times(1);
+
+  log::debug("[TESTING] firstDevice notifies the available context are back");
+  DeviceContextsUpdate(firstDevice, types::kLeAudioDirectionSink, audio_contexts, audio_contexts);
+
+  log::debug("[TESTING] ProcessHciNotifSetupIsoDataPath. Expect StatusReportCb to be not called");
+  LeAudioGroupStateMachine::Get()->AttachToStream(group, firstDevice,
+                                                  {.sink = {media_ccid}, .source = {}});
+
+  testing::Mock::VerifyAndClearExpectations(mock_iso_manager_);
+
+  auto* firstDeviceAse = firstDevice->GetFirstActiveAseByDirection(types::kLeAudioDirectionSink);
+  ASSERT_NE(nullptr, firstDeviceAse);
+
+  /* Expect the Data Path in CONFIGURING state */
+  ASSERT_EQ(types::DataPathState::CONFIGURING, firstDeviceAse->data_path_state);
+
+  // let us decide when the ISO Data Path Setup Complete and CIS Disconnection Complete event
+  do_not_send_setup_iso_data_path_event_ = true;
+  do_not_send_remove_iso_data_path_event_ = true;
+  do_not_send_cis_disconnected_event_ = true;
+
+  EXPECT_CALL(*mock_iso_manager_, RemoveIsoDataPath(cis_conn_handle, _)).Times(1);
+  EXPECT_CALL(*mock_iso_manager_, DisconnectCis(cis_conn_handle, _)).Times(1);
+
+  log::debug("[TESTING] first device performs autonomous ASE state transition to Releasing state");
+  InjectAseStateNotification(firstDeviceAse, firstDevice, group, ascs::kAseStateReleasing, nullptr);
+
+  log::debug("[TESTING] ProcessHciNotifSetupIsoDataPath");
+  LeAudioGroupStateMachine::Get()->ProcessHciNotifSetupIsoDataPath(group, firstDevice, 0,
+                                                                   cis_conn_handle);
+
+  bluetooth::hci::iso_manager::cis_disconnected_evt cis_disconnected_evt = {
+          .reason = HCI_ERR_PEER_USER,
+          .cig_id = group_id,
+          .cis_conn_hdl = firstDeviceAse->cis_conn_hdl,
+  };
+  log::debug("[TESTING] controller reports first device CIS has been disconnected eventually");
+  LeAudioGroupStateMachine::Get()->ProcessHciNotifCisDisconnected(group, firstDevice,
+                                                                  &cis_disconnected_evt);
+
+  testing::Mock::VerifyAndClearExpectations(mock_iso_manager_);
+
+  log::debug("[TESTING] ProcessHciNotifRemoveIsoDataPath");
+  LeAudioGroupStateMachine::Get()->ProcessHciNotifRemoveIsoDataPath(group, firstDevice, 0,
+                                                                    cis_conn_handle);
+  ASSERT_EQ(types::DataPathState::IDLE, firstDeviceAse->data_path_state);
 }
 
 TEST_F(StateMachineTest, testDoNotQoSConfiguredIfNotStreaming) {
