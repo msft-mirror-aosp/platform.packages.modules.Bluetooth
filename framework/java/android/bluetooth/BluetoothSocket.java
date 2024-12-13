5e988322d265b0f237ae915dcad5bc8dc8c901c1
Bhakthavatsala Raghavendra <bhaktha@google.com>
2024-12-13 21:15:10 +0000

    Throw an IOException on EOF instead of returning error
    This ensures consistent behavior across RFCOMM, L2CAP and LECOC sockets
    
    Bug: 383671392
    Test: mmm packages/modules/Bluetooth
    Flag: Exempt, minor change
    
    Change-Id: I97d52faff6e065f04f578c78eacb8865bbb82305

--- framework/java/android/bluetooth/BluetoothSocket.java
+++ framework/java/android/bluetooth/BluetoothSocket.java
@@ -978,7 +978,7 @@ public final class BluetoothSocket implements Closeable {
             if (mL2capBuffer.remaining() == 0) {
                 if (VDBG) Log.v(TAG, "l2cap buffer empty, refilling...");
                 if (fillL2capRxBuffer() == -1) {
-                    return -1;
+                    throw new IOException("bt socket closed, read return: " + ret);
                 }
             }
             if (bytesToRead > mL2capBuffer.remaining()) {
