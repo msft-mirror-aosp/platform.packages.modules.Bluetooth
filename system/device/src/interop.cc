miyu-xu/packages-modules-Bluetooth

floss: interop JBL Flip 5/6 to disregard HFP
JBL Flip 5/6 does not support HFP by specs and so is it in practice, but
it advertises the profile and that could cause trouble for Floss media
policy.

This CL adds an interop to disregard the HFP profile when advertised by
these headsets.

Bug: 381009230
Tag: #floss
Test: mmm packages/modules/Bluetooth
Flag: EXEMPT, enum adding and Floss-only logic change

Change-Id: I9079b0a877a3070cd1cbe7db9df202495e72f594

system/device/src/interop.cc
@@ -393,6 +393,7 @@ static const char* interop_feature_string_(const interop_feature_t feature) {
     CASE_RETURN_STR(INTEROP_HID_PREF_CONN_ZERO_LATENCY);
     CASE_RETURN_STR(INTEROP_HOGP_LONG_REPORT);
     CASE_RETURN_STR(INTEROP_HOGP_FORCE_MTU_EXCHANGE);
+    CASE_RETURN_STR(INTEROP_DISREGARD_HFP_PROFILE);
   }
   return UNKNOWN_INTEROP_FEATURE;
 }

system/gd/rust/topshim/hfp/hfp_shim.cc
@@ -383,6 +383,10 @@ bool interop_insert_call_when_sco_start(RawAddress addr) {
   return interop_match_addr(interop_feature_t::INTEROP_INSERT_CALL_WHEN_SCO_START, &addr);
 }
 
+bool interop_disregard_hfp_profile(const char* name) {
+  return interop_match_name(interop_feature_t::INTEROP_DISREGARD_HFP_PROFILE, name);
+}
+
 }  // namespace rust
 }  // namespace topshim
 }  // namespace bluetooth

system/gd/rust/topshim/hfp/hfp_shim.h
@@ -59,6 +59,7 @@ private:
 
 std::unique_ptr<HfpIntf> GetHfpProfile(const unsigned char* btif);
 bool interop_insert_call_when_sco_start(RawAddress addr);
+bool interop_disregard_hfp_profile(const char* name);
 
 }  // namespace rust
 }  // namespace topshim

system/gd/rust/topshim/src/profiles/hfp.rs
@@ -192,6 +192,7 @@ pub mod ffi {
 
         unsafe fn GetHfpProfile(btif: *const u8) -> UniquePtr<HfpIntf>;
         unsafe fn interop_insert_call_when_sco_start(bt_addr: RawAddress) -> bool;
+        unsafe fn interop_disregard_hfp_profile(name: *const c_char) -> bool;
         fn init(self: Pin<&mut HfpIntf>) -> i32;
         fn connect(self: Pin<&mut HfpIntf>, bt_addr: RawAddress) -> u32;
         fn connect_audio(
@@ -263,6 +264,12 @@ pub mod ffi {
 pub fn interop_insert_call_when_sco_start(bt_addr: RawAddress) -> bool {
     //Call an unsafe function in c++. This is necessary for bridge C++ interop API with floss(rust).
     unsafe { return ffi::interop_insert_call_when_sco_start(bt_addr) }
+}
+
+pub fn interop_disregard_hfp_profile(name: String) -> bool {
+    let c_name = std::ffi::CString::new(name).unwrap();
+    // Call an unsafe function in c++. This is necessary for bridge C++ interop API with floss(rust).
+    unsafe { return ffi::interop_disregard_hfp_profile(c_name.as_ptr()) }
 }
 
 pub type TelephonyDeviceStatus = ffi::TelephonyDeviceStatus;

msft-mirror-aosp/platform.packages.modules.Bluetooth

system/device/src/interop.cc
@@ -393,6 +393,7 @@ static const char* interop_feature_string_(const interop_feature_t feature) {
     CASE_RETURN_STR(INTEROP_HID_PREF_CONN_ZERO_LATENCY);
     CASE_RETURN_STR(INTEROP_HOGP_LONG_REPORT);
     CASE_RETURN_STR(INTEROP_HOGP_FORCE_MTU_EXCHANGE);
+    CASE_RETURN_STR(INTEROP_DISABLE_HF_PROFILE);
   }
   return UNKNOWN_INTEROP_FEATURE;
 }

system/gd/rust/topshim/hfp/hfp_shim.cc
@@ -383,6 +383,10 @@ bool interop_insert_call_when_sco_start(RawAddress addr) {
   return interop_match_addr(interop_feature_t::INTEROP_INSERT_CALL_WHEN_SCO_START, &addr);
 }
 
+bool interop_disable_hf_profile(const char* name) {
+  return interop_match_name(interop_feature_t::INTEROP_DISABLE_HF_PROFILE, name);
+}
+
 }  // namespace rust
 }  // namespace topshim
 }  // namespace bluetooth

system/gd/rust/topshim/hfp/hfp_shim.h
@@ -59,6 +59,7 @@ private:
 
 std::unique_ptr<HfpIntf> GetHfpProfile(const unsigned char* btif);
 bool interop_insert_call_when_sco_start(RawAddress addr);
+bool interop_disable_hf_profile(const char* name);
 
 }  // namespace rust
 }  // namespace topshim

system/gd/rust/topshim/src/profiles/hfp.rs
@@ -192,6 +192,7 @@ pub mod ffi {
 
         unsafe fn GetHfpProfile(btif: *const u8) -> UniquePtr<HfpIntf>;
         unsafe fn interop_insert_call_when_sco_start(bt_addr: RawAddress) -> bool;
+        unsafe fn interop_disable_hf_profile(name: *const c_char) -> bool;
         fn init(self: Pin<&mut HfpIntf>) -> i32;
         fn connect(self: Pin<&mut HfpIntf>, bt_addr: RawAddress) -> u32;
         fn connect_audio(
@@ -263,6 +264,12 @@ pub mod ffi {
 pub fn interop_insert_call_when_sco_start(bt_addr: RawAddress) -> bool {
     //Call an unsafe function in c++. This is necessary for bridge C++ interop API with floss(rust).
     unsafe { return ffi::interop_insert_call_when_sco_start(bt_addr) }
+}
+
+pub fn interop_disable_hf_profile(name: String) -> bool {
+    let c_name = std::ffi::CString::new(name).unwrap();
+    // Call an unsafe function in c++. This is necessary for bridge C++ interop API with floss(rust).
+    unsafe { return ffi::interop_disable_hf_profile(c_name.as_ptr()) }
 }
 
 pub type TelephonyDeviceStatus = ffi::TelephonyDeviceStatus;