miyu-xu/packages-modules-Bluetooth

Pass all parameters of DistanceMeasurementResult to app
Flag: com.android.bluetooth.flags.channel_sounding_25q2_apis
Bug: 367409858
Bug: 340301310
Test: m com.android.btservices
Change-Id: I8288c41e79ffb2b38bd081502f28c54f142fb827

system/gd/hal/ranging_hal.h
@@ -281,9 +281,15 @@ struct ProcedureDataV2 {
 
 struct RangingResult {
   double result_meters_;
+  double error_meters_;
+
   // A normalized value from 0 (low confidence) to 100 (high confidence) representing the confidence
   // of estimated distance. The value is -1 when unavailable.
   int8_t confidence_level_;
+  double delay_spread_meters_;
+  int8_t detected_attack_level_;
+  double velocity_meters_per_second_;
+  int64_t timestamp_nanos_;
 };
 
 class RangingHalCallback {

system/gd/hal/ranging_hal_android.cc
@@ -73,10 +73,11 @@ class BluetoothChannelSoundingSessionTracker : public BnBluetoothChannelSounding
 public:
   BluetoothChannelSoundingSessionTracker(uint16_t connection_handle,
                                          RangingHalCallback* ranging_hal_callback,
-                                         bool for_vendor_specific_reply)
+                                         bool for_vendor_specific_reply, RangingHalVersion hal_ver)
       : connection_handle_(connection_handle),
         ranging_hal_callback_(ranging_hal_callback),
-        for_vendor_specific_reply_(for_vendor_specific_reply) {}
+        for_vendor_specific_reply_(for_vendor_specific_reply),
+        hal_ver_(hal_ver) {}
 
   ::ndk::ScopedAStatus onOpened(::aidl::android::hardware::bluetooth::ranging::Reason in_reason) {
     log::info("connection_handle 0x{:04x}, reason {}", connection_handle_, (uint16_t)in_reason);
@@ -103,8 +104,15 @@ public:
     log::verbose("resultMeters {}", in_result.resultMeters);
     hal::RangingResult ranging_result = {
             .result_meters_ = in_result.resultMeters,
+            .error_meters_ = in_result.errorMeters,
             .confidence_level_ = in_result.confidenceLevel,
+            .delay_spread_meters_ = in_result.delaySpreadMeters,
+            .detected_attack_level_ = static_cast<int8_t>(in_result.detectedAttackLevel),
+            .velocity_meters_per_second_ = in_result.velocityMetersPerSecond,
     };
+    if (hal_ver_ == V_2) {
+      ranging_result.timestamp_nanos_ = in_result.timestampNanos;
+    }
     ranging_hal_callback_->OnResult(connection_handle_, ranging_result);
     return ::ndk::ScopedAStatus::ok();
   }
@@ -129,6 +137,7 @@ private:
   uint16_t connection_handle_;
   RangingHalCallback* ranging_hal_callback_;
   bool for_vendor_specific_reply_;
+  RangingHalVersion hal_ver_;
 };
 
 class RangingHalAndroid : public RangingHal {
@@ -167,7 +176,7 @@ public:
               connection_handle, att_handle, vendor_specific_data.size());
     session_trackers_[connection_handle] =
             ndk::SharedRefBase::make<BluetoothChannelSoundingSessionTracker>(
-                    connection_handle, ranging_hal_callback_, false);
+                    connection_handle, ranging_hal_callback_, false, hal_ver_);
     BluetoothChannelSoundingParameters parameters;
     parameters.aclHandle = connection_handle;
     parameters.role = aidl::android::hardware::bluetooth::ranging::Role::INITIATOR;
@@ -200,7 +209,7 @@ public:
     log::info("connection_handle 0x{:04x}", connection_handle);
     session_trackers_[connection_handle] =
             ndk::SharedRefBase::make<BluetoothChannelSoundingSessionTracker>(
-                    connection_handle, ranging_hal_callback_, true);
+                    connection_handle, ranging_hal_callback_, true, hal_ver_);
     BluetoothChannelSoundingParameters parameters;
     parameters.aclHandle = connection_handle;
     parameters.role = aidl::android::hardware::bluetooth::ranging::Role::REFLECTOR;

system/main/shim/distance_measurement_manager.cc
@@ -25,6 +25,7 @@
 #include "stack/include/acl_api.h"
 #include "stack/include/main_thread.h"
 
+using bluetooth::hci::DistanceMeasurementDetectedAttackLevel;
 using bluetooth::hci::DistanceMeasurementErrorCode;
 using bluetooth::hci::DistanceMeasurementMethod;
 using namespace bluetooth;
@@ -118,14 +119,17 @@ public:
                                    uint32_t error_centimeter, int azimuth_angle,
                                    int error_azimuth_angle, int altitude_angle,
                                    int error_altitude_angle, uint64_t elapsedRealtimeNanos,
-                                   int8_t confidence_level,
+                                   int8_t confidence_level, double delayedSpreadCentimeters,
+                                   DistanceMeasurementDetectedAttackLevel detectedAttackLevel,
+                                   double velocityCentimetersPerSecond,
                                    DistanceMeasurementMethod method) override {
-    do_in_jni_thread(base::BindOnce(&::DistanceMeasurementCallbacks::OnDistanceMeasurementResult,
-                                    base::Unretained(distance_measurement_callbacks_),
-                                    bluetooth::ToRawAddress(address), centimeter, error_centimeter,
-                                    azimuth_angle, error_azimuth_angle, altitude_angle,
-                                    error_altitude_angle, elapsedRealtimeNanos, confidence_level,
-                                    static_cast<uint8_t>(method)));
+    do_in_jni_thread(base::BindOnce(
+            &::DistanceMeasurementCallbacks::OnDistanceMeasurementResult,
+            base::Unretained(distance_measurement_callbacks_), bluetooth::ToRawAddress(address),
+            centimeter, error_centimeter, azimuth_angle, error_azimuth_angle, altitude_angle,
+            error_altitude_angle, elapsedRealtimeNanos, confidence_level, delayedSpreadCentimeters,
+            static_cast<uint8_t>(detectedAttackLevel), velocityCentimetersPerSecond,
+            static_cast<uint8_t>(method)));
   }
 
   void OnRasFragmentReady(bluetooth::hci::Address address, uint16_t procedure_counter, bool is_last,

msft-mirror-aosp/platform.packages.modules.Bluetooth

system/gd/hal/ranging_hal.h
@@ -281,9 +281,15 @@ struct ProcedureDataV2 {
 
 struct RangingResult {
   double result_meters_;
+  double error_meters_;
+
   // A normalized value from 0 (low confidence) to 100 (high confidence) representing the confidence
   // of estimated distance. The value is -1 when unavailable.
   int8_t confidence_level_;
+  double delay_spread_meters_;
+  uint8_t detected_attack_level_;
+  double velocity_meters_per_second_;
+  int64_t elapsed_timestamp_nanos_;
 };
 
 class RangingHalCallback {

system/gd/hal/ranging_hal_android.cc
@@ -73,10 +73,11 @@ class BluetoothChannelSoundingSessionTracker : public BnBluetoothChannelSounding
 public:
   BluetoothChannelSoundingSessionTracker(uint16_t connection_handle,
                                          RangingHalCallback* ranging_hal_callback,
-                                         bool for_vendor_specific_reply)
+                                         bool for_vendor_specific_reply, RangingHalVersion hal_ver)
       : connection_handle_(connection_handle),
         ranging_hal_callback_(ranging_hal_callback),
-        for_vendor_specific_reply_(for_vendor_specific_reply) {}
+        for_vendor_specific_reply_(for_vendor_specific_reply),
+        hal_ver_(hal_ver) {}
 
   ::ndk::ScopedAStatus onOpened(::aidl::android::hardware::bluetooth::ranging::Reason in_reason) {
     log::info("connection_handle 0x{:04x}, reason {}", connection_handle_, (uint16_t)in_reason);
@@ -103,8 +104,15 @@ public:
     log::verbose("resultMeters {}", in_result.resultMeters);
     hal::RangingResult ranging_result = {
             .result_meters_ = in_result.resultMeters,
+            .error_meters_ = in_result.errorMeters,
             .confidence_level_ = in_result.confidenceLevel,
+            .delay_spread_meters_ = in_result.delaySpreadMeters,
+            .detected_attack_level_ = static_cast<uint8_t>(in_result.detectedAttackLevel),
+            .velocity_meters_per_second_ = in_result.velocityMetersPerSecond,
     };
+    if (hal_ver_ == V_2) {
+      ranging_result.elapsed_timestamp_nanos_ = in_result.timestampNanos;
+    }
     ranging_hal_callback_->OnResult(connection_handle_, ranging_result);
     return ::ndk::ScopedAStatus::ok();
   }
@@ -129,6 +137,7 @@ private:
   uint16_t connection_handle_;
   RangingHalCallback* ranging_hal_callback_;
   bool for_vendor_specific_reply_;
+  RangingHalVersion hal_ver_;
 };
 
 class RangingHalAndroid : public RangingHal {
@@ -167,7 +176,7 @@ public:
               connection_handle, att_handle, vendor_specific_data.size());
     session_trackers_[connection_handle] =
             ndk::SharedRefBase::make<BluetoothChannelSoundingSessionTracker>(
-                    connection_handle, ranging_hal_callback_, false);
+                    connection_handle, ranging_hal_callback_, false, hal_ver_);
     BluetoothChannelSoundingParameters parameters;
     parameters.aclHandle = connection_handle;
     parameters.role = aidl::android::hardware::bluetooth::ranging::Role::INITIATOR;
@@ -200,7 +209,7 @@ public:
     log::info("connection_handle 0x{:04x}", connection_handle);
     session_trackers_[connection_handle] =
             ndk::SharedRefBase::make<BluetoothChannelSoundingSessionTracker>(
-                    connection_handle, ranging_hal_callback_, true);
+                    connection_handle, ranging_hal_callback_, true, hal_ver_);
     BluetoothChannelSoundingParameters parameters;
     parameters.aclHandle = connection_handle;
     parameters.role = aidl::android::hardware::bluetooth::ranging::Role::REFLECTOR;

system/main/shim/distance_measurement_manager.cc
@@ -25,6 +25,7 @@
 #include "stack/include/acl_api.h"
 #include "stack/include/main_thread.h"
 
+using bluetooth::hci::DistanceMeasurementDetectedAttackLevel;
 using bluetooth::hci::DistanceMeasurementErrorCode;
 using bluetooth::hci::DistanceMeasurementMethod;
 using namespace bluetooth;
@@ -117,15 +118,18 @@ public:
   void OnDistanceMeasurementResult(bluetooth::hci::Address address, uint32_t centimeter,
                                    uint32_t error_centimeter, int azimuth_angle,
                                    int error_azimuth_angle, int altitude_angle,
-                                   int error_altitude_angle, uint64_t elapsedRealtimeNanos,
-                                   int8_t confidence_level,
+                                   int error_altitude_angle, uint64_t elapsed_realtime_nanos,
+                                   int8_t confidence_level, double delay_spread_meters,
+                                   DistanceMeasurementDetectedAttackLevel detected_attack_level,
+                                   double velocity_meters_per_second,
                                    DistanceMeasurementMethod method) override {
-    do_in_jni_thread(base::BindOnce(&::DistanceMeasurementCallbacks::OnDistanceMeasurementResult,
-                                    base::Unretained(distance_measurement_callbacks_),
-                                    bluetooth::ToRawAddress(address), centimeter, error_centimeter,
-                                    azimuth_angle, error_azimuth_angle, altitude_angle,
-                                    error_altitude_angle, elapsedRealtimeNanos, confidence_level,
-                                    static_cast<uint8_t>(method)));
+    do_in_jni_thread(base::BindOnce(
+            &::DistanceMeasurementCallbacks::OnDistanceMeasurementResult,
+            base::Unretained(distance_measurement_callbacks_), bluetooth::ToRawAddress(address),
+            centimeter, error_centimeter, azimuth_angle, error_azimuth_angle, altitude_angle,
+            error_altitude_angle, elapsed_realtime_nanos, confidence_level, delay_spread_meters,
+            static_cast<uint8_t>(detected_attack_level), velocity_meters_per_second,
+            static_cast<uint8_t>(method)));
   }
 
   void OnRasFragmentReady(bluetooth::hci::Address address, uint16_t procedure_counter, bool is_last,