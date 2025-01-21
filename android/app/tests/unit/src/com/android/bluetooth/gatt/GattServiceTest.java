miyu-xu/packages-modules-Bluetooth

GattService: Limit adapterService GATT notification mechanism
With this CL only finder application will be notified to AdapterService
when GATT connection is made.

That means, only this application will be able to connect to LeAudio
device over GATT without making this device auto Active in
AudioFramework.

Bug: 388179850
Bug: 389274300
Flag: com.android.bluetooth.flags.allow_gatt_connect_from_the_apps_without_making_leaudio_device_active
Test: atest GattServiceTest
Change-Id: I3652429800957c79b4e7c5a9d56b77dcb6629285

android/app/tests/unit/src/com/android/bluetooth/gatt/GattServiceTest.java
@@ -36,6 +36,7 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Process;
 import android.platform.test.flag.junit.SetFlagsRule;
 import android.provider.Settings;
 import android.test.mock.MockContentProvider;
@@ -278,6 +279,14 @@ public class GattServiceTest {
         boolean opportunistic = false;
         int phy = 3;
 
+        AttributionSource testAttributeSource =
+                new AttributionSource.Builder(Process.SYSTEM_UID)
+                        .setPid(Process.myPid())
+                        .setDeviceId(Context.DEVICE_ID_DEFAULT)
+                        .setPackageName("com.google.android.gmscore")
+                        .setAttributionTag("com.google.android.gms.findmydevice")
+                        .build();
+
         mService.clientConnect(
                 clientIf,
                 address,
@@ -286,7 +295,7 @@ public class GattServiceTest {
                 transport,
                 opportunistic,
                 phy,
-                mAttributionSource);
+                testAttributeSource);
 
         verify(mAdapterService).notifyDirectLeGattClientConnect(anyInt(), any());
         verify(mNativeInterface)
@@ -306,6 +315,14 @@ public class GattServiceTest {
         boolean opportunistic = false;
         int phy = 3;
 
+        AttributionSource testAttributeSource =
+                new AttributionSource.Builder(Process.SYSTEM_UID)
+                        .setPid(Process.myPid())
+                        .setDeviceId(Context.DEVICE_ID_DEFAULT)
+                        .setPackageName("com.google.android.gmscore")
+                        .setAttributionTag("com.google.android.gms.findmydevice")
+                        .build();
+
         mService.clientConnect(
                 clientIf,
                 address,
@@ -314,7 +331,7 @@ public class GattServiceTest {
                 transport,
                 opportunistic,
                 phy,
-                mAttributionSource);
+                testAttributeSource);
 
         verify(mAdapterService).notifyDirectLeGattClientConnect(anyInt(), any());
         verify(mNativeInterface)
@@ -336,6 +353,14 @@ public class GattServiceTest {
         boolean opportunistic = false;
         int phy = 3;
 
+        AttributionSource testAttributeSource =
+                new AttributionSource.Builder(Process.SYSTEM_UID)
+                        .setPid(Process.myPid())
+                        .setDeviceId(Context.DEVICE_ID_DEFAULT)
+                        .setPackageName("com.google.android.gmscore")
+                        .setAttributionTag("com.google.android.gms.findmydevice")
+                        .build();
+
         mService.clientConnect(
                 clientIf,
                 address,
@@ -344,7 +369,7 @@ public class GattServiceTest {
                 transport,
                 opportunistic,
                 phy,
-                mAttributionSource);
+                testAttributeSource);
 
         verify(mAdapterService).notifyDirectLeGattClientConnect(anyInt(), any());
         verify(mNativeInterface)

msft-mirror-aosp/platform.packages.modules.Bluetooth

android/app/tests/unit/src/com/android/bluetooth/gatt/GattServiceTest.java
@@ -36,6 +36,7 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Process;
 import android.platform.test.flag.junit.SetFlagsRule;
 import android.provider.Settings;
 import android.test.mock.MockContentProvider;
@@ -278,6 +279,14 @@ public class GattServiceTest {
         boolean opportunistic = false;
         int phy = 3;
 
+        AttributionSource testAttributeSource =
+                new AttributionSource.Builder(Process.SYSTEM_UID)
+                        .setPid(Process.myPid())
+                        .setDeviceId(Context.DEVICE_ID_DEFAULT)
+                        .setPackageName("com.google.android.gms")
+                        .setAttributionTag("com.google.android.gms.findmydevice")
+                        .build();
+
         mService.clientConnect(
                 clientIf,
                 address,
@@ -286,7 +295,7 @@ public class GattServiceTest {
                 transport,
                 opportunistic,
                 phy,
-                mAttributionSource);
+                testAttributeSource);
 
         verify(mAdapterService).notifyDirectLeGattClientConnect(anyInt(), any());
         verify(mNativeInterface)
@@ -306,6 +315,14 @@ public class GattServiceTest {
         boolean opportunistic = false;
         int phy = 3;
 
+        AttributionSource testAttributeSource =
+                new AttributionSource.Builder(Process.SYSTEM_UID)
+                        .setPid(Process.myPid())
+                        .setDeviceId(Context.DEVICE_ID_DEFAULT)
+                        .setPackageName("com.google.android.gms")
+                        .setAttributionTag("com.google.android.gms.findmydevice")
+                        .build();
+
         mService.clientConnect(
                 clientIf,
                 address,
@@ -314,7 +331,7 @@ public class GattServiceTest {
                 transport,
                 opportunistic,
                 phy,
-                mAttributionSource);
+                testAttributeSource);
 
         verify(mAdapterService).notifyDirectLeGattClientConnect(anyInt(), any());
         verify(mNativeInterface)
@@ -336,6 +353,14 @@ public class GattServiceTest {
         boolean opportunistic = false;
         int phy = 3;
 
+        AttributionSource testAttributeSource =
+                new AttributionSource.Builder(Process.SYSTEM_UID)
+                        .setPid(Process.myPid())
+                        .setDeviceId(Context.DEVICE_ID_DEFAULT)
+                        .setPackageName("com.google.android.gms")
+                        .setAttributionTag("com.google.android.gms.findmydevice")
+                        .build();
+
         mService.clientConnect(
                 clientIf,
                 address,
@@ -344,7 +369,7 @@ public class GattServiceTest {
                 transport,
                 opportunistic,
                 phy,
-                mAttributionSource);
+                testAttributeSource);
 
         verify(mAdapterService).notifyDirectLeGattClientConnect(anyInt(), any());
         verify(mNativeInterface)