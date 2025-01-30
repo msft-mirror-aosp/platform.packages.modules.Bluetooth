miyu-xu/packages-modules-Bluetooth

Save SDP information in p_scb/client_cb
To prepare for RFCOMM metrics, save relevant SDP data in a struct within
p_scb or client_cb for BTA_AG and BTA_HF_CLIENT respectively

Bug: 374989690
Test: m com.android.btservices
Flag: EXEMPT metrics no-op
Change-Id: I6d1e3dee79e230f3409fda4014dfe69e892942e0

system/bta/ag/bta_ag_sdp.cc
@@ -38,6 +38,7 @@
 #include "bta_sys.h"
 #include "btif/include/btif_config.h"
 #include "btm_api_types.h"
+#include "common/time_util.h"
 #include "device/include/interop.h"
 #include "device/include/interop_config.h"
 #include "internal_include/bt_target.h"
@@ -106,6 +107,9 @@ static void bta_ag_sdp_cback(tSDP_STATUS status, uint8_t idx) {
       event = BTA_AG_DISC_INT_RES_EVT;
     }
     tBTA_AG_DATA disc_result = {.disc_result = {.status = status}};
+    p_scb->sdp.status = (status == tSDP_STATUS::SDP_SUCCESS) ? tBTA_JV_STATUS::SUCCESS
+                                                             : tBTA_JV_STATUS::FAILURE;
+    p_scb->sdp.sdp_end_ms = common::time_gettimeofday_us();
     do_in_main_thread(base::BindOnce(&bta_ag_sm_execute_by_handle, idx, event, disc_result));
   }
 }
@@ -522,6 +526,8 @@ void bta_ag_do_disc(tBTA_AG_SCB* p_scb, tBTA_SERVICE_MASK service) {
     return;
   }
 
+  p_scb->sdp.sdp_initiated = true;
+
   /* allocate buffer for sdp database */
   p_scb->p_disc_db = (tSDP_DISCOVERY_DB*)osi_malloc(BTA_AG_DISC_BUF_SIZE);
   /* set up service discovery database; attr happens to be attr_list len */
@@ -530,6 +536,7 @@ void bta_ag_do_disc(tBTA_AG_SCB* p_scb, tBTA_SERVICE_MASK service) {
     if (get_legacy_stack_sdp_api()->service.SDP_ServiceSearchAttributeRequest(
                 p_scb->peer_addr, p_scb->p_disc_db,
                 bta_ag_sdp_cback_tbl[bta_ag_scb_to_idx(p_scb) - 1])) {
+      p_scb->sdp.sdp_start_ms = common::time_gettimeofday_us() / 1000;
       return;
     } else {
       log::error("failed to start SDP discovery for {}", p_scb->peer_addr);

system/bta/hf_client/bta_hf_client_sdp.cc
@@ -37,6 +37,7 @@
 #include "bta/sys/bta_sys.h"
 #include "bta_hfp_api.h"
 #include "common/bind.h"
+#include "common/time_util.h"
 #include "internal_include/bt_target.h"
 #include "osi/include/allocator.h"
 #include "sdp_status.h"
@@ -86,6 +87,10 @@ static void bta_hf_client_sdp_cback(tBTA_HF_CLIENT_CB* client_cb, const RawAddre
   p_buf->hdr.event = event;
   p_buf->hdr.layer_specific = client_cb->handle;
   p_buf->status = status;
+
+  client_cb->sdp.status =
+          (status == tSDP_STATUS::SDP_SUCCESS) ? tBTA_JV_STATUS::SUCCESS : tBTA_JV_STATUS::FAILURE;
+  client_cb->sdp.sdp_end_ms = common::time_gettimeofday_us() / 1000;
 
   bta_sys_sendmsg(p_buf);
 }
@@ -388,6 +393,9 @@ void bta_hf_client_do_disc(tBTA_HF_CLIENT_CB* client_cb) {
     msg.hdr.layer_specific = client_cb->handle;
     bta_hf_client_sm_execute(BTA_HF_CLIENT_DISC_FAIL_EVT, &msg);
   }
+
+  // Successfully started - record time SDP started
+  client_cb->sdp.sdp_start_ms = common::time_gettimeofday_us() / 1000;
 }
 
 /*******************************************************************************

msft-mirror-aosp/platform.packages.modules.Bluetooth

system/bta/ag/bta_ag_sdp.cc
@@ -38,6 +38,7 @@
 #include "bta_sys.h"
 #include "btif/include/btif_config.h"
 #include "btm_api_types.h"
+#include "common/time_util.h"
 #include "device/include/interop.h"
 #include "device/include/interop_config.h"
 #include "internal_include/bt_target.h"
@@ -106,6 +107,9 @@ static void bta_ag_sdp_cback(tSDP_STATUS status, uint8_t idx) {
       event = BTA_AG_DISC_INT_RES_EVT;
     }
     tBTA_AG_DATA disc_result = {.disc_result = {.status = status}};
+    p_scb->sdp_metrics.status = (status == tSDP_STATUS::SDP_SUCCESS) ? tBTA_JV_STATUS::SUCCESS
+                                                                     : tBTA_JV_STATUS::FAILURE;
+    p_scb->sdp_metrics.sdp_end_ms = common::time_gettimeofday_us();
     do_in_main_thread(base::BindOnce(&bta_ag_sm_execute_by_handle, idx, event, disc_result));
   }
 }
@@ -522,6 +526,8 @@ void bta_ag_do_disc(tBTA_AG_SCB* p_scb, tBTA_SERVICE_MASK service) {
     return;
   }
 
+  p_scb->sdp_metrics.sdp_initiated = true;
+
   /* allocate buffer for sdp database */
   p_scb->p_disc_db = (tSDP_DISCOVERY_DB*)osi_malloc(BTA_AG_DISC_BUF_SIZE);
   /* set up service discovery database; attr happens to be attr_list len */
@@ -530,6 +536,7 @@ void bta_ag_do_disc(tBTA_AG_SCB* p_scb, tBTA_SERVICE_MASK service) {
     if (get_legacy_stack_sdp_api()->service.SDP_ServiceSearchAttributeRequest(
                 p_scb->peer_addr, p_scb->p_disc_db,
                 bta_ag_sdp_cback_tbl[bta_ag_scb_to_idx(p_scb) - 1])) {
+      p_scb->sdp_metrics.sdp_start_ms = common::time_gettimeofday_us() / 1000;
       return;
     } else {
       log::error("failed to start SDP discovery for {}", p_scb->peer_addr);

system/bta/hf_client/bta_hf_client_sdp.cc
@@ -37,6 +37,7 @@
 #include "bta/sys/bta_sys.h"
 #include "bta_hfp_api.h"
 #include "common/bind.h"
+#include "common/time_util.h"
 #include "internal_include/bt_target.h"
 #include "osi/include/allocator.h"
 #include "sdp_status.h"
@@ -86,6 +87,10 @@ static void bta_hf_client_sdp_cback(tBTA_HF_CLIENT_CB* client_cb, const RawAddre
   p_buf->hdr.event = event;
   p_buf->hdr.layer_specific = client_cb->handle;
   p_buf->status = status;
+
+  client_cb->sdp_metrics.status =
+          (status == tSDP_STATUS::SDP_SUCCESS) ? tBTA_JV_STATUS::SUCCESS : tBTA_JV_STATUS::FAILURE;
+  client_cb->sdp_metrics.sdp_end_ms = common::time_gettimeofday_us() / 1000;
 
   bta_sys_sendmsg(p_buf);
 }
@@ -388,6 +393,9 @@ void bta_hf_client_do_disc(tBTA_HF_CLIENT_CB* client_cb) {
     msg.hdr.layer_specific = client_cb->handle;
     bta_hf_client_sm_execute(BTA_HF_CLIENT_DISC_FAIL_EVT, &msg);
   }
+
+  // Successfully started - record time SDP started
+  client_cb->sdp_metrics.sdp_start_ms = common::time_gettimeofday_us() / 1000;
 }
 
 /*******************************************************************************