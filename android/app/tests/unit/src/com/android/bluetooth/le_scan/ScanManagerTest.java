miyu-xu/packages-modules-Bluetooth

Change default number of trackable advertisements for onFound filter
The current default number of trackable advertisements set for each
onFound filter is 'MATCH_NUM_MAX_ADVERTISEMENT', which is 'Max hardware allows / 2'. This is exhausting
resources and causing errors. Therefore, changing the default to
'MATCH_NUM_FEW_ADVERTISEMENT', and also changing the value of
'MATCH_NUM_MAX_ADVERTISEMENT' to 'Max / 4'.

Bug: 386727721
Bug: 389568695
Test: atest ScanManagerTest, ScanSettingsTest
Change-Id: I39cb19fd6e299c9b74c441fc3da35f232cc8ec74

android/app/tests/unit/src/com/android/bluetooth/le_scan/ScanManagerTest.java
@@ -72,6 +72,7 @@ import android.os.SystemProperties;
 import android.os.WorkSource;
 import android.os.test.TestLooper;
 import android.platform.test.annotations.EnableFlags;
+import android.platform.test.annotations.RequiresFlagsEnabled;
 import android.platform.test.flag.junit.SetFlagsRule;
 import android.provider.Settings;
 import android.test.mock.MockContentProvider;
@@ -136,6 +137,7 @@ public class ScanManagerTest {
     private static final int DEFAULT_BATCH_SCAN_REPORT_DELAY_MS = 100;
     private static final int DEFAULT_NUM_OFFLOAD_SCAN_FILTER = 16;
     private static final int DEFAULT_BYTES_OFFLOAD_SCAN_RESULT_STORAGE = 4096;
+    private static final int DEFAULT_TOTAL_NUM_OF_TRACKABLE_ADVERTISEMENTS = 32;
     private static final int TEST_SCAN_QUOTA_COUNT = 5;
     private static final String TEST_APP_NAME = "Test";
     private static final String TEST_PACKAGE_NAME = "com.test.package";
@@ -188,6 +190,9 @@ public class ScanManagerTest {
         doReturn(SCAN_MODE_SCREEN_OFF_BALANCED_INTERVAL_MS)
                 .when(mAdapterService)
                 .getScreenOffBalancedIntervalMillis();
+        doReturn(DEFAULT_TOTAL_NUM_OF_TRACKABLE_ADVERTISEMENTS)
+                .when(mAdapterService)
+                .getTotalNumOfTrackableAdvertisements();
 
         TestUtils.mockGetSystemService(
                 mAdapterService, Context.LOCATION_SERVICE, LocationManager.class, mLocationManager);
@@ -1854,6 +1859,16 @@ public class ScanManagerTest {
                 BluetoothProfile.HID_HOST, STATE_DISCONNECTED, STATE_CONNECTING);
         mLooper.dispatchAll();
         assertThat(mScanManager.mProfilesConnecting).isEqualTo(3);
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_CHANGE_DEFAULT_TRACKABLE_ADV_NUMBER)
+    public void getNumOfTrackingAdvertisements_withDefaultScanSettings() {
+        ScanSettings scanSettings;
+        scanSettings = new ScanSettings.Builder().build();
+
+        assertThat(mScanManager.mScanNative.getNumOfTrackingAdvertisements(scanSettings))
+                .isEqualTo(ScanSettings.MATCH_NUM_FEW_ADVERTISEMENT);
     }
 
     private void phytest(int phy, int expectedPhy) {

msft-mirror-aosp/platform.packages.modules.Bluetooth

Bug: 391958027

android/app/tests/unit/src/com/android/bluetooth/le_scan/ScanManagerTest.java
@@ -68,6 +68,7 @@ import android.os.Bundle;
 import android.os.Message;
 import android.os.ParcelUuid;
 import android.os.WorkSource;
+import android.platform.test.annotations.DisableFlags;
 import android.platform.test.annotations.EnableFlags;
 import android.platform.test.flag.junit.SetFlagsRule;
 import android.provider.Settings;
@@ -135,6 +136,7 @@ public class ScanManagerTest {
     private static final int DEFAULT_BATCH_SCAN_REPORT_DELAY_MS = 100;
     private static final int DEFAULT_NUM_OFFLOAD_SCAN_FILTER = 16;
     private static final int DEFAULT_BYTES_OFFLOAD_SCAN_RESULT_STORAGE = 4096;
+    private static final int DEFAULT_TOTAL_NUM_OF_TRACKABLE_ADVERTISEMENTS = 32;
     private static final int TEST_SCAN_QUOTA_COUNT = 5;
     private static final String TEST_APP_NAME = "Test";
     private static final String TEST_PACKAGE_NAME = "com.test.package";
@@ -183,6 +185,9 @@ public class ScanManagerTest {
         doReturn(SCAN_MODE_SCREEN_OFF_BALANCED_INTERVAL_MS)
                 .when(mAdapterService)
                 .getScreenOffBalancedIntervalMillis();
+        doReturn(DEFAULT_TOTAL_NUM_OF_TRACKABLE_ADVERTISEMENTS)
+                .when(mAdapterService)
+                .getTotalNumOfTrackableAdvertisements();
 
         TestUtils.mockGetSystemService(
                 mAdapterService, Context.LOCATION_SERVICE, LocationManager.class, mLocationManager);
@@ -1852,6 +1857,32 @@ public class ScanManagerTest {
                 BluetoothProfile.HID_HOST, STATE_DISCONNECTED, STATE_CONNECTING);
         mLooper.dispatchAll();
         assertThat(mScanManager.mProfilesConnecting).isEqualTo(3);
+    }
+
+    @Test
+    @DisableFlags(Flags.FLAG_CHANGE_DEFAULT_TRACKABLE_ADV_NUMBER)
+    public void getNumOfTrackingAdvertisements_withMaxTrackable_flagEnabled() {
+        ScanSettings scanSettings;
+        scanSettings =
+                new ScanSettings.Builder()
+                        .setNumOfMatches(ScanSettings.MATCH_NUM_MAX_ADVERTISEMENT)
+                        .build();
+
+        assertThat(mScanManager.mScanNative.getNumOfTrackingAdvertisements(scanSettings))
+                .isEqualTo(DEFAULT_TOTAL_NUM_OF_TRACKABLE_ADVERTISEMENTS / 2);
+    }
+
+    @Test
+    @EnableFlags(Flags.FLAG_CHANGE_DEFAULT_TRACKABLE_ADV_NUMBER)
+    public void getNumOfTrackingAdvertisements_withMaxTrackable_flagDisabled() {
+        ScanSettings scanSettings;
+        scanSettings =
+                new ScanSettings.Builder()
+                        .setNumOfMatches(ScanSettings.MATCH_NUM_MAX_ADVERTISEMENT)
+                        .build();
+
+        assertThat(mScanManager.mScanNative.getNumOfTrackingAdvertisements(scanSettings))
+                .isEqualTo(DEFAULT_TOTAL_NUM_OF_TRACKABLE_ADVERTISEMENTS / 4);
     }
 
     private void phytest(int phy, int expectedPhy) {