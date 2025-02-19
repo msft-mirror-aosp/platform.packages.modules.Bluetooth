miyu-xu/packages-modules-Bluetooth

HAL: Add a sysprop to read MSFT vendor specific opcode
Using MGMT to read the MSFT vendor specific opcode from the
kernel is too BlueZ specific. Replace with a sysprop instead.

Bug: 390425327
Bug: 365787977
Flag: com.android.bluetooth.flags.le_scan_msft_support
Test: mmm packages/modules/Bluetooth
Change-Id: I88bd30f1c1b81ab1299af0459aef4e7596183db0

system/gd/hal/hci_hal_android.cc
@@ -14,7 +14,9 @@
  * limitations under the License.
  */
 
+#include <android_bluetooth_sysprop.h>
 #include <bluetooth/log.h>
+#include <com_android_bluetooth_flags.h>
 
 #include <future>
 #include <mutex>
@@ -25,7 +27,6 @@
 #include "hal/hci_hal.h"
 #include "hal/link_clocker.h"
 #include "hal/snoop_logger.h"
-#include "os/mgmt.h"
 
 namespace bluetooth::hal {
 
@@ -159,7 +160,10 @@ public:
   }
 
   uint16_t getMsftOpcode() override {
-    return os::Management::getInstance().getVendorSpecificCode(MGMT_VS_OPCODE_MSFT);
+    if (com::android::bluetooth::flags::le_scan_msft_support()) {
+      return android::sysprop::bluetooth::Ble::msft_vendor_opcode().value_or(0);
+    }
+    return 0;
   }
 
 protected:

msft-mirror-aosp/platform.packages.modules.Bluetooth

system/gd/hal/hci_hal_android.cc
@@ -14,7 +14,9 @@
  * limitations under the License.
  */
 
+#include <android_bluetooth_sysprop.h>
 #include <bluetooth/log.h>
+#include <com_android_bluetooth_flags.h>
 
 #include <future>
 #include <mutex>
@@ -25,7 +27,6 @@
 #include "hal/hci_hal.h"
 #include "hal/link_clocker.h"
 #include "hal/snoop_logger.h"
-#include "os/mgmt.h"
 
 namespace bluetooth::hal {
 
@@ -159,7 +160,10 @@ public:
   }
 
   uint16_t getMsftOpcode() override {
-    return os::Management::getInstance().getVendorSpecificCode(MGMT_VS_OPCODE_MSFT);
+    if (com::android::bluetooth::flags::le_scan_msft_support()) {
+      return android::sysprop::bluetooth::Hci::msft_vendor_opcode().value_or(0);
+    }
+    return 0;
   }
 
 protected: