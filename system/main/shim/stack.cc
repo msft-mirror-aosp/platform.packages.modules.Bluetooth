miyu-xu/packages-modules-Bluetooth

Move SnoopLogger out of the module registry
Bug: 333555245
Test: m com.android.bt
Flag: EXEMPT, no logical change
Change-Id: I77d3b062ccdc1f508856dd616af64c043277f0e7

system/main/shim/stack.cc
@@ -69,6 +69,7 @@ struct Stack::impl {
   Acl* acl_ = nullptr;
   metrics::CounterMetrics* counter_metrics_ = nullptr;
   storage::StorageModule* storage_ = nullptr;
+  hal::SnoopLogger* snoop_logger_ = nullptr;
 };
 
 Stack::Stack() { pimpl_ = std::make_shared<Stack::impl>(); }
@@ -90,6 +91,7 @@ void Stack::StartEverything() {
 
     pimpl_->counter_metrics_ = new metrics::CounterMetrics(new Handler(stack_thread_));
     pimpl_->storage_ = new storage::StorageModule(new Handler(stack_thread_));
+    pimpl_->snoop_logger_ = new hal::SnoopLogger(new Handler(stack_thread_));
 
 #if TARGET_FLOSS
     modules.add<sysprops::SyspropsModule>();
@@ -223,6 +225,12 @@ storage::StorageModule* Stack::GetStorage() const {
   return pimpl_->storage_;
 }
 
+hal::SnoopLogger* Stack::GetSnoopLogger() const {
+  std::lock_guard<std::recursive_mutex> lock(mutex_);
+  log::assert_that(is_running_, "assert failed: is_running_");
+  return pimpl_->snoop_logger_;
+}
+
 os::Handler* Stack::GetHandler() {
   std::lock_guard<std::recursive_mutex> lock(mutex_);
   log::assert_that(is_running_, "assert failed: is_running_");
@@ -249,12 +257,14 @@ void Stack::Dump(int fd, std::promise<void> promise) const {
 void Stack::handle_start_up(ModuleList* modules, std::promise<void> promise) {
   pimpl_->counter_metrics_->Start();
   pimpl_->storage_->Start();
+  pimpl_->snoop_logger_ ->Start();
   registry_.Start(modules, stack_thread_);
   promise.set_value();
 }
 
 void Stack::handle_shut_down(std::promise<void> promise) {
   registry_.StopAll();
+  pimpl_->snoop_logger_ ->Stop();
   pimpl_->storage_->Stop();
   pimpl_->counter_metrics_->Stop();
   promise.set_value();

msft-mirror-aosp/platform.packages.modules.Bluetooth

system/main/shim/stack.cc
@@ -69,6 +69,7 @@ struct Stack::impl {
   Acl* acl_ = nullptr;
   metrics::CounterMetrics* counter_metrics_ = nullptr;
   storage::StorageModule* storage_ = nullptr;
+  hal::SnoopLogger* snoop_logger_ = nullptr;
 };
 
 Stack::Stack() { pimpl_ = std::make_shared<Stack::impl>(); }
@@ -90,6 +91,7 @@ void Stack::StartEverything() {
 
     pimpl_->counter_metrics_ = new metrics::CounterMetrics(new Handler(stack_thread_));
     pimpl_->storage_ = new storage::StorageModule(new Handler(stack_thread_));
+    pimpl_->snoop_logger_ = new hal::SnoopLogger(new Handler(stack_thread_));
 
 #if TARGET_FLOSS
     modules.add<sysprops::SyspropsModule>();
@@ -116,11 +118,12 @@ void Stack::StartEverything() {
     WakelockManager::Get().Acquire();
   }
 
+  is_running_ = true;
+
   std::promise<void> promise;
   auto future = promise.get_future();
   management_handler_->Post(common::BindOnce(&Stack::handle_start_up, common::Unretained(this),
                                              &modules, std::move(promise)));
-  is_running_ = true;
   auto init_status = future.wait_for(
           std::chrono::milliseconds(get_gd_stack_timeout_ms(/* is_start = */ true)));
 
@@ -223,6 +226,12 @@ storage::StorageModule* Stack::GetStorage() const {
   return pimpl_->storage_;
 }
 
+hal::SnoopLogger* Stack::GetSnoopLogger() const {
+  std::lock_guard<std::recursive_mutex> lock(mutex_);
+  log::assert_that(is_running_, "assert failed: is_running_");
+  return pimpl_->snoop_logger_;
+}
+
 os::Handler* Stack::GetHandler() {
   std::lock_guard<std::recursive_mutex> lock(mutex_);
   log::assert_that(is_running_, "assert failed: is_running_");
@@ -249,12 +258,14 @@ void Stack::Dump(int fd, std::promise<void> promise) const {
 void Stack::handle_start_up(ModuleList* modules, std::promise<void> promise) {
   pimpl_->counter_metrics_->Start();
   pimpl_->storage_->Start();
+  pimpl_->snoop_logger_->Start();
   registry_.Start(modules, stack_thread_);
   promise.set_value();
 }
 
 void Stack::handle_shut_down(std::promise<void> promise) {
   registry_.StopAll();
+  pimpl_->snoop_logger_->Stop();
   pimpl_->storage_->Stop();
   pimpl_->counter_metrics_->Stop();
   promise.set_value();